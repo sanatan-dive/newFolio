5eb0ea9f3c363bd0c1d642b67567b2314034c033
sanatan-dive <sanatansharma352@gmail.com>
2025-03-26 01:13:18 +0530

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import "../../public/favicon.png"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Sanatan's Portfolio",
   description: "showcasing my work",
-  icons: "/favicon.png"
+  icons: "../../public/favicon.png"
 };
 
 export default function RootLayout({
